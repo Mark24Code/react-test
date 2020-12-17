4743397caccb788cb599ab1f8ecdf75bd4a4ed12
mark1.zhang <mark1.zhang@liulishuo.com>
2020-12-17 15:07:35 +0800

    svg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import Logo2 from './logo2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo logo1" alt="logo" />
+
+        {/* 起作用的是透传颜色 */}
+        <Logo2 className="logo2" color="red"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
