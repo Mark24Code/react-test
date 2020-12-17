2d6cf2ab1643b4e9b3deb136bd16c90be7bfd09e
mark1.zhang <mark1.zhang@liulishuo.com>
2020-12-17 15:09:32 +0800

    add doc

---
+++ docs/precache-manifest.4a2fbf05e49b1f08025dca2bc9f0e58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7f174166fd6ac07947d41fd7d0f1be",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "ae861a492deec9eb64ba",
+    "url": "/react-test/static/css/main.ac9c369a.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae861a492deec9eb64ba",
+    "url": "/react-test/static/js/main.a2c5f4a7.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
